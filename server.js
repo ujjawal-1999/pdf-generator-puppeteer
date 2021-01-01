05aeca18b226c66c3948656e6c9ec96c45160891
Ujjawal Jain <jainujjawal1999@gmail.com>
2021-01-01 23:08:56 +0530

    URL to PDF converter

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/", async (req, res) => {
+  const url = req.body.url;
+  const format = req.body.format || "A0";
+  const path = `${req.body.path}.pdf` || "webpage.pdf";
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+
+  const webpage = await browser.newPage();
+
+  await webpage.goto(url, {
+    waitUntil: "networkidle0",
+  });
+
+  const dimensions = await webpage.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      scale: window.devicePixelRatio,
+    };
+  });
+
+  const pdf = await webpage.pdf({
+    ...dimensions,
+    printBackground: true,
+    format: format,
+    landscape: true,
+    path: path,
+    margin: {
+      top: "20px",
+      bottom: "40px",
+      left: "30px",
+      right: "30px",
+    },
+  });
+  // await browser.close();
+  res.contentType("application/pdf");
+  res.send(pdf);
+});
+
+app.listen(port, () => {
+  console.log("Server Up and Running");
+});
